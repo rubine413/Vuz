08f200cbf0eecc81496be0076da3dfb7e20309a7
rubine413 <rubine.lee@qq.com>
2021-12-15 17:19:23 +0800

    icon

--- vue.config.js
+++ vue.config.js
@@ -6,20 +6,15 @@ module.exports = {
     index: {
       entry: 'examples/main.js',
       template: 'public/index.html',
-      filename: 'index.html'
-    }
+      filename: 'index.html',
+    },
   },
   chainWebpack: config => {
     // vue默认@指向src目录，这里要修正为examples，另外新增一个~指向packages
     config.resolve.alias.set('@', path.resolve('examples')).set('~', path.resolve('packages'))
     // lib目录是组件库最终打包好存放的地方，不需要eslint检查
     // examples/docs是存放md文档的地方，也不需要eslint检查
-    config.module
-      .rule('eslint')
-      .exclude.add(path.resolve('lib'))
-      .end()
-      .exclude.add(path.resolve('examples/docs'))
-      .end()
+    config.module.rule('eslint').exclude.add(path.resolve('lib')).end().exclude.add(path.resolve('examples/docs')).end()
     // packages和examples目录需要加入编译
     config.module
       .rule('js')
@@ -33,5 +28,5 @@ module.exports = {
         // 修改它的选项...
         return options
       })
-  }
+  },
 }
